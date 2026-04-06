9c99ee4039b2e4f4b594d08e8da195004f0134c3
Ghanshyam-9946 <dhoteghanshyam9@gmail.com>
2026-04-06 13:09:54 +0530

    update transporter to use service gmail

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -2,9 +2,7 @@ const nodemailer = require("nodemailer");
 
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, 
+  service: "gmail",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
