eec0a6a462ffddcb8c9eb09854ecca38d9613860
Ghanshyam-9946 <dhoteghanshyam9@gmail.com>
2026-04-14 12:27:52 +0530

    panda

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -2,7 +2,9 @@ const nodemailer = require("nodemailer");
 
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for 587
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
